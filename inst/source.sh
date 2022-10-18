272e2abf43c17aedd49478e7f00661adf9da9199
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2022-10-18 13:39:50 -0700

    Updated the schemas.

--- inst/source.sh
+++ inst/source.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-curl -L https://github.com/ArtifactDB/calcite-schemas/releases/download/2022-10-16/bundle.tar.gz > bundle.tar.gz
+curl -L https://github.com/ArtifactDB/calcite-schemas/releases/download/2022-10-18/bundle.tar.gz > bundle.tar.gz
 rm -rf schemas
 tar -xvf bundle.tar.gz
 mv resolved schemas
